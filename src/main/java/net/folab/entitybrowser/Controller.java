1188b69c32fbf60cef81ea2c93a516e0d62e68d6
leafriend <leafriend@gmail.com>
2015-06-10 01:55:18 +0900

    Add exceptoinHandlers, managing methods & test.

--- src/main/java/net/folab/entitybrowser/Controller.java
+++ src/main/java/net/folab/entitybrowser/Controller.java
@@ -1,5 +1,8 @@
 package net.folab.entitybrowser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Controls models & views. This is responsible to C from MVC pattern. This
  * class is singleton.
@@ -14,12 +17,48 @@ public final class Controller {
     private static final Controller INSTANCE = new Controller();
 
     /**
+     * Holds exception handlers which will be invoked when exception is thrown
+     * in user-interface thread.
+     */
+    private transient final List<ExceptoinHandler> exceptoinHandlers = new ArrayList<ExceptoinHandler>();
+
+    /**
      * Returns instance of this class. This is a part of singleton pattern.
      *
      * @return instance of this class. It's always same instance.
      */
     public static Controller getInstance() {
         return INSTANCE;
+    }
+
+    /**
+     * Add exception handler. Added handler will be invoked when exception is
+     * thrown in user-interface thread.
+     *
+     * @param handler
+     *            to add
+     */
+    public void addExceptoinHandler(final ExceptoinHandler handler) {
+        exceptoinHandlers.add(handler);
+    }
+
+    /**
+     * Remove exception handler. Removed handler is not invoked when exception
+     * is thrown in user-interface thread.
+     *
+     * @param handler
+     *            to remove
+     */
+    public void removeExceptoinHandler(final ExceptoinHandler handler) {
+        exceptoinHandlers.remove(handler);
+    }
+
+    /**
+     * Remove all exception handler. Nothing is happened even an exception is
+     * thrown in user-interface thread after clearing.
+     */
+    public void clearExceptoinHandler() {
+        exceptoinHandlers.clear();
     }
 
     /**
--- src/test/java/net/folab/entitybrowser/ControllerTest.java
+++ src/test/java/net/folab/entitybrowser/ControllerTest.java
@@ -1,10 +1,14 @@
 package net.folab.entitybrowser;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -20,6 +24,66 @@ public class ControllerTest {
         Controller second = Controller.getInstance();
         assertThat("getInstance() should return exactly same intance always",
                 second, is(sameInstance(first)));
+
+    }
+
+    @Test
+    public void testExceptionHandlers() throws Exception {
+
+        // Prepare test
+
+        Controller controller = Controller.getInstance();
+
+        Field field = Controller.class.getDeclaredField("exceptoinHandlers");
+        field.setAccessible(true);
+        @SuppressWarnings("unchecked")
+        List<ExceptoinHandler> exceptoinHandlers = (List<ExceptoinHandler>) field
+                .get(controller);
+
+        // Test initial state
+
+        assertThat("exceptoinHandlers should not be null", exceptoinHandlers,
+                is(not(nullValue())));
+
+        assertThat("exceptoinHandlers should be empty at first",
+                exceptoinHandlers.size(), is(0));
+
+        // Test adding
+
+        ExceptoinHandler handler = e -> {
+        };
+
+        controller.addExceptoinHandler(handler);
+
+        assertThat("exceptoinHandlers should contain added handler",
+                exceptoinHandlers, contains(handler));
+
+        assertThat("exceptoinHandlers size should be 1",
+                exceptoinHandlers.size(), is(1));
+
+        // Test removing
+
+        controller.removeExceptoinHandler(handler);
+
+        assertThat("exceptoinHandlers should not contain added handler",
+                exceptoinHandlers, not(contains(handler)));
+
+        assertThat("exceptoinHandlers size should be 0",
+                exceptoinHandlers.size(), is(0));
+
+        // Test clearing
+
+        controller.addExceptoinHandler(handler);
+
+        controller.clearExceptoinHandler();
+
+        assertThat("exceptoinHandlers size should be 0 after clear",
+                exceptoinHandlers.size(), is(0));
+
+        controller.clearExceptoinHandler();
+
+        assertThat("exceptoinHandlers size should be 0 after clear again",
+                exceptoinHandlers.size(), is(0));
 
     }
 
